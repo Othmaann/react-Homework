c2dab037de4f3ebbea5092c211be7620ae780679
Othmaann <91528006+Othmaann@users.noreply.github.com>
2022-10-16 19:25:14 +0200

    test

--- react/react/react-template/src/components/budget/Budget.jsx
+++ react/react/react-template/src/components/budget/Budget.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
+Deposit = (event) => {
+  event.preventDefault();
+  this.inputName = event.target.value;
+  localStorage.setItem("inputName", this.inputName);
+};
 
 export default class Deposit extends React.Component {
+  inputName = "";
   render() {
-
+    this.inputName = localStorage.getItem("inputName");
     return (
       <form required>
-        <input type="number" required></input>
+        <input type="number" required onChange={this.Deposit} ></input>
         <button onClick={this.Deposit}>Deposit</button>
+        <button onClick={() => this.clickMeThree(this.inputName)}>
+          Click me Three
+        </button>
+        <h1>hi {this.inputName}</h1>
       </form>
     );
   }
