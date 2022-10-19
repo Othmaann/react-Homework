0da7febceebfeeabd4b71aa80a9bc252f344d4c1
Othmaann <91528006+Othmaann@users.noreply.github.com>
2022-10-19 14:51:26 +0200

    new

--- react/react/react-template/src/App.js
+++ react/react/react-template/src/App.js
@@ -3,19 +3,30 @@ import Deposit from "./components/budget/Budget";
 import Withdraw from "./components/withdraw/Withdraw";
 
 class App extends React.Component {
-
-  Mymoney = {
-    "budget" : Number,
-    "Withdraw" : Number,
-    "balance" : Number
-  }
+  total = 0;
+  getTotalNumber = (total) => {
+    if (localStorage.getItem("total")) {
+      localStorage.setItem(
+        "total",
+        parseInt(localStorage.getItem("total")) + total
+      );
+    } else {
+      localStorage.setItem("total", total);
+    }
+    window.location.reload();
+  };
 
   render() {
+    const item = localStorage.getItem("total")
+      ? localStorage.getItem("total")
+      : 0;
     return (
-      
       <div>
-        <Deposit />
+        <Deposit
+          getTotalNumber={this.getTotalNumber}
+        />
         <Withdraw />
+        <h3>{item}</h3>
       </div>
     );
   }
--- react/react/react-template/src/components/budget/Budget.jsx
+++ react/react/react-template/src/components/budget/Budget.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-Deposit = (event) => {
-  event.preventDefault();
-  this.inputName = event.target.value;
-  localStorage.setItem("inputName", this.inputName);
-};
-
 export default class Deposit extends React.Component {
-  inputName = "";
+  inputDepositAmount = 0;
+  total = 0;
+  getDespoitAmount = (e) => {
+    this.inputDepositAmount = parseInt(e.target.value);
+  };
+
+  onSubmitHandel = (e) => {
+    e.preventDefault();
+    this.total = this.total + this.inputDepositAmount;
+    this.props.getTotalNumber(this.total);
+  };
   render() {
-    this.inputName = localStorage.getItem("inputName");
     return (
-      <form required>
-        <input type="number" required onChange={this.Deposit} ></input>
-        <button onClick={this.Deposit}>Deposit</button>
-        <button onClick={() => this.clickMeThree(this.inputName)}>
-          Click me Three
-        </button>
-        <h1>hi {this.inputName}</h1>
+      <form required className="myForm" onSubmit={this.onSubmitHandel}>
+        <input
+          required
+          placeholder="desposit amount:"
+          type="number"
+          onChange={this.getDespoitAmount}
+        />
+        <input type="submit" value="Submit" />
       </form>
     );
   }
